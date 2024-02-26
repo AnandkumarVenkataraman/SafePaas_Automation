cb89733b1690792a7f6c1cc0bab2d82f7a9b66f7
debabrata.patel <debabrata.patel@APP05-KATALON>
2024-02-26 08:21:07 -0600

    Commit_Feb26_1

---
+++ Test Suites/Administration_Company Settings.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Administration_Company Settings</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>10e1e807-86e9-4b93-81b7-31161a05235b</testSuiteGuid>
+   <testCaseLink>
+      <guid>c10b87fe-0284-413e-b470-ec6bf03fd25e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Administration/Administration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
