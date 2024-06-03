f4de147fb0b8590171cc8b40e5964a1be80fc114
debabrata.patel <debabrata.patel@APP05-KATALON>
2024-06-03 08:23:01 -0500

    commit

---
+++ Test Suites/EAM_June03_TestOps/2_RuleCreation_ImportRules.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>2_RuleCreation_ImportRules</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3c4ce978-87e7-46c3-a3f4-91fd0d72fa8f</testSuiteGuid>
+   <testCaseLink>
+      <guid>bc994c6b-015d-4a74-8251-8563ea76d167</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EAM/2_RuleCreation_Import</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>7df8a602-dc9b-4a09-bf2a-d3693b9a66cd</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
