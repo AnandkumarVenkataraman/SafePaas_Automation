48bb57acd0611fc89e9fe5c3efbc86cd78645e3e
mariaisabel.brum <mariaisabel.brum@APP05-KATALON>
2024-07-04 21:49:35 -0500

    commit for CrossApp SOD

---
+++ Test Suites/CrossApp SOD.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CrossApp SOD</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d39dab1d-587e-4555-bd64-989d34d83dad</testSuiteGuid>
+   <testCaseLink>
+      <guid>ebedb496-b042-41e8-bfc0-fb3f50c7861a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CrossApp SOD/CrossApp SOD</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
