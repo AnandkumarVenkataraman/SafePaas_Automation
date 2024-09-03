10c549e27ff64b5c65f84ba62038d387498ce121
mariaisabel.brum <mariaisabel.brum@APP05-KATALON>
2024-09-03 12:08:57 -0500

    new commit

--- Scripts/MonitorPaaS/MonitorPaaS_CloudERP/3_ManageMonitor_Snapshot_ResultSets_Approval/Script1720505124379.groovy
+++ Scripts/MonitorPaaS/MonitorPaaS_CloudERP/3_ManageMonitor_Snapshot_ResultSets_Approval/Script1720505124379.groovy
@@ -89,7 +89,7 @@ WebUI.takeFullPageScreenshot()
 
 WebUI.scrollToPosition(0, 0)
 
-WebUI.navigateToUrl('https://us21n.safepaas.com/monitor/f?p=1110:94:::NO::P94_TYPE,P94_USER,P94_SNAPSHOT,P94_RUN_ID,P94_CONTROL_ID:APPROVER,100912,315959,41312,324025')
+WebUI.navigateToUrl('https://us21n.safepaas.com/monitor/f?p=1110:94:::NO::P94_TYPE,P94_USER,P94_SNAPSHOT,P94_RUN_ID,P94_CONTROL_ID:APPROVER,100912,316235,41512,324145')
 
 WebUI.delay(5)
 
--- Scripts/MonitorPaaS/MonitorPaaS_CloudERP/6_MonitorPaaS_Submitted by Reviewer/Script1720505196275.groovy
+++ Scripts/MonitorPaaS/MonitorPaaS_CloudERP/6_MonitorPaaS_Submitted by Reviewer/Script1720505196275.groovy
@@ -44,7 +44,7 @@ WebUI.click(findTestObject('Object Repository/MonitorPaaS_IncidentReport/Submitt
 
 WebUI.comment('click on Login button')
 
-WebUI.navigateToUrl('https://us21n.safepaas.com/monitor/f?p=1110:94:::NO::P94_TYPE,P94_USER,P94_SNAPSHOT,P94_RUN_ID,P94_CONTROL_ID:REVIEWER,66812854,315959,41312,324025')
+WebUI.navigateToUrl('https://us21n.safepaas.com/monitor/f?p=1110:94:::NO::P94_TYPE,P94_USER,P94_SNAPSHOT,P94_RUN_ID,P94_CONTROL_ID:APPROVER,100912,316235,41512,324145')
 
 WebUI.comment('launch the Incident report URL for reviewer')
 
