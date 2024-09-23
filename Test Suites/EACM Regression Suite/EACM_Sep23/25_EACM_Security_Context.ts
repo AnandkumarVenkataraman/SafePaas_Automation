2aa8722807d53aaf61e7098ab100725f3367a8b3
mariaisabel.brum <mariaisabel.brum@APP05-KATALON>
2024-09-23 10:37:57 -0500

    new commit sep23

---
+++ Test Suites/EACM Regression Suite/EACM_Sep23/25_EACM_Security_Context.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>25_EACM_Security_Context</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e8fd29f1-cf2e-4e98-a1a1-4328c1abd0e7</testSuiteGuid>
+   <testCaseLink>
+      <guid>8ca281c8-14c7-454c-b26b-221e5ec04b9b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/EACM/25_EACM_Security_Context</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
