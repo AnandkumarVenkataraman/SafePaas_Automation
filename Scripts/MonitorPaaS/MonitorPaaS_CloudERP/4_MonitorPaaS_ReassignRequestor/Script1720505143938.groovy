3219079e8ff1691ad7d0379998acebea5bc00919
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2024-11-07 00:17:52 -0600

    Nov7 - MTP

--- Scripts/MonitorPaaS/MonitorPaaS_CloudERP/4_MonitorPaaS_ReassignRequestor/Script1720505143938.groovy
+++ Scripts/MonitorPaaS/MonitorPaaS_CloudERP/4_MonitorPaaS_ReassignRequestor/Script1720505143938.groovy
@@ -44,7 +44,7 @@ WebUI.click(findTestObject('Object Repository/MonitorPaaS_IncidentReport/Reassig
 
 WebUI.comment('click on Login button')
 
-WebUI.navigateToUrl('https://us21n.safepaas.com/monitor/f?p=1110:93:::NO::P93_TYPE,P93_USER,P93_SNAPSHOT,P93_RUN_ID,P93_CONTROL_ID:REQUESTOR,MARIAB,316731,42212,324625')
+WebUI.navigateToUrl('https://us21n.safepaas.com/monitor/f?p=1110:94:::NO::P94_TYPE,P94_USER,P94_SNAPSHOT,P94_RUN_ID,P94_CONTROL_ID:APPROVER,100912,316927,42653,325386')
 
 WebUI.comment('launch the Incident report URL for Reassign requestor')
 
