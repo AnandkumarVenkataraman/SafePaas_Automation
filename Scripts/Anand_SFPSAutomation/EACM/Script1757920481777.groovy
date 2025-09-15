d8e2940d99d8a0e0a86ebdc6965b9e76c430a43b
Anandkumar Venkataraman <anandkumar.venkataraman@safepaas.com.com>
2025-09-15 06:06:54 -0500

    EACM Setup v1

--- Scripts/Anand_SFPSAutomation/EACM/Script1757920481777.groovy
+++ Scripts/Anand_SFPSAutomation/EACM/Script1757920481777.groovy
@@ -16,20 +16,163 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
 import java.text.SimpleDateFormat as SimpleDateFormat
 import java.util.Date as Date
 import java.time.LocalDate as LocalDate
 import java.time.format.DateTimeFormatter as DateTimeFormatter
 
-//EACM Setups
+WebUI.openBrowser('')
 
-//Enterprise IT Mapping Setup
-//Operations vs. IDM
+WebUI.navigateToUrl('https://us21n.safepaas.com/monitor/f?p=1000:101::::::')
+WebUI.maximizeWindow()
 
-//Single Env. survey -> Role-based
+WebUI.setText(findTestObject('Object Repository/Page_Login/input_Summer 2025_P101_COMPANY (3)'), 'KAT')
 
-//Single Env. survey -> Identity-based
+WebUI.setText(findTestObject('Object Repository/Page_Login/input_OR CONTINUE WITH_P101_USERNAME (8)'), 'KATADMIN')
 
-//Multi Env. survey
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Login/input_New_P101_PASSWORD (8)'), 'Ioqy8emDrxi5mkiNQATzxQ==')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Login/button_Reset Password_loginBtn (1)'))
+
+//Verify the EACM Setups in Company Settings
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_DataPaaS_a-TreeView-toggle (9)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Administration_a-TreeView-toggle (6)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/a_Company Settings_a-TreeView-label (1)'))
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Company/div_DataProbe_apex-item-option'), 0)
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Company/span_Company_t-Tabs-label'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Settings/input_Settings_R307838823214507554_ig_toolb_058319'), 'Enterprise Access Certification')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Settings/button_Settings_a-Button a-Toolbar-item'))
+WebUI.takeScreenshot()
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Settings/button_concat(Search for , , Enterprise Acc_57ba07'), FailureHandling.OPTIONAL)
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Settings/span_Subscription_t-Tabs-label'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Product_P34_PRODUCT'), 'UAC', 
+    true)
+
+WebUI.selectOptionByIndex(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    1)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Template Name_P34_TEMPLATE_NAME'), 
+    '714', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Cancelling Notification', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Template Name_P34_TEMPLATE_NAME'), 
+    '710', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Delegation Notification', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Template Name_P34_TEMPLATE_NAME'), 
+    '711', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Escalation Notification', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Template Name_P34_TEMPLATE_NAME'), 
+    '712', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Generic', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Template Name_P34_TEMPLATE_NAME'), 
+    '707', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Identity Group User Mapping Generic Notification', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Identity Group User Mapping Reminder Notification', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'POST Process Error', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Template Name_P34_TEMPLATE_NAME'), 
+    '708', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Reassignment Notification', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Template Name_P34_TEMPLATE_NAME'), 
+    '709', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Reminder Notification', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Template Name_P34_TEMPLATE_NAME'), 
+    '713', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Survey Advance Intimation', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Email Configuration/select_Email Category_P34_EMAIL_CATEGORY'), 
+    'Survey job failure', true)
+
+WebUI.selectOptionByIndex(findTestObject('Object Repository/Page_Email Configuration/select_Product_P34_PRODUCT'), 0)
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Email Configuration/a_Inbox_t-Button t-Button--icon t-Button--h_469732'))
+
+//Add code to verify pop-up later
+
+//Manage Env.
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_DataPaaS_a-TreeView-toggle (10)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Administration_a-TreeView-toggle (7)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/a_Manage Company_a-TreeView-label'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Manage Environment/input_Create_R258165063082313757_search_field (3)'),
+	'EBizApp_JDBC')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Environment/button_Create_R258165063082313757_search_button (1)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Environment/button_concat(Row text contains , , EBizApp_824ca1'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Manage Environment/input_Create_R258165063082313757_search_field (3)'),
+	'Test_Environment')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Environment/button_Create_R258165063082313757_search_button (1)'))
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Manage Environment/td_-_u-tC (1)'), 'A')
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Manage Environment/td_A_u-tC (1)'), 'Oracle Cloud ERP 20x')
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Manage Environment/td_Oracle Cloud ERP 20x_u-tL'), 'Operations')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Environment/button_concat(Row text contains , , EBizApp_824ca1'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Manage Environment/input_Create_R258165063082313757_search_field (3)'),
+	'JDE_Env')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Environment/button_Create_R258165063082313757_search_button (1)'))
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Manage Environment/td_-_u-tC (1)'), 'A')
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Manage Environment/td_Oracle Cloud ERP 20x_u-tL'), 'Operations')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Environment/button_concat(Row text contains , , EBizApp_824ca1'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Manage Environment/input_Create_R258165063082313757_search_field (3)'),
+	'EBizApp_JDBC_Env')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Environment/button_Create_R258165063082313757_search_button (1)'))
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Manage Environment/td_-_u-tC (1)'), 'A')
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Manage Environment/td_A_u-tC_1'), 'EBS R12.2')
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Manage Environment/td_Oracle Cloud ERP 20x_u-tL'), 'Operations')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Environment/button_concat(Row text contains , , EBizApp_824ca1'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Environment/a_Inbox_t-Button t-Button--icon t-Button--h_469732 (1)'))
+
--- Scripts/Anand_SFPSAutomation/Old/EACM_Old/EACM_Security/Script1748058575427.groovy
+++ Scripts/Anand_SFPSAutomation/Old/EACM_Old/EACM_Security/Script1748058575427.groovy
@@ -35,20 +35,20 @@ WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/
 WebUI.setEncryptedText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/input_New_P101_PASSWORD (9)'), 
     'Ioqy8emDrxi5mkiNQATzxQ==')
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/button_Login (8)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/button_Login (8)'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_DataPaaS_a-TreeView-toggle (1)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_DataPaaS_a-TreeView-toggle (1)'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Administration_a-TreeView-toggle (1)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Administration_a-TreeView-toggle (1)'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Manage Company (1)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Manage Company (1)'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Company/span_Users Access'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Company/span_Users Access'))
 
 WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/input_Submit_R259422667645149962_ig_toolbar_d69900'), 
     'ANANDK1')
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/button_Go'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/button_Go'))
 
 WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/td_ANANDK1'), 
     'ANANDK1')
@@ -58,22 +58,22 @@ WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/P
 
 WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/td_-'), '-')
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/button_concat(Search for , , ANANDK1, , )_a_9456c8'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/button_concat(Search for , , ANANDK1, , )_a_9456c8'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/span_Help_a-TreeView-toggle'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/span_Help_a-TreeView-toggle'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/span_Manage Load Query_a-TreeView-toggle'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/span_Manage Load Query_a-TreeView-toggle'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/span_Manage User Access_a-TreeView-toggle'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/span_Manage User Access_a-TreeView-toggle'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/a_Manage EACM Roles'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Users Application Access/a_Manage EACM Roles'))
 
 WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage EACM Roles/input_Create_R635717313875105759_search_field'), 
     'EACM_ReadOnly')
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage EACM Roles/button_Go'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage EACM Roles/button_Go'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage EACM Roles/img_EACM_ReadOnly_apex-edit-page'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage EACM Roles/img_EACM_ReadOnly_apex-edit-page'))
 
 WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_CreateEdit Role/label_Manage Certification - View only Acce_ab60a0'), 
     'Manage Certification - View only Access for Manage Certification')
@@ -84,29 +84,29 @@ WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/P
 WebUI.verifyElementPresent(findTestObject('Object Repository/Anand_SFPSAutomation/Page_CreateEdit Role/label_Manage Certification - Able to Create_e99237'), 
     0)
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_CreateEdit Role/button_Cancel'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_CreateEdit Role/button_Cancel'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage EACM Roles/button_concat(Row text contains , , EACM_Re_2f0ff7'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage EACM Roles/button_concat(Row text contains , , EACM_Re_2f0ff7'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage EACM Roles/a_Assign EACM Roles'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage EACM Roles/a_Assign EACM Roles'))
 
 WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Assign EACM Roles/input_Create_R506984600294913364_search_field'), 
     'ANANDK1')
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Assign EACM Roles/button_Go'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Assign EACM Roles/button_Go'))
 
 WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Assign EACM Roles/td_ANANDK1'), 'ANANDK1')
 
 WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Assign EACM Roles/td_EACM_ReadOnly'), 
     'EACM_ReadOnly')
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Assign EACM Roles/button_concat(Row text contains , , ANANDK1_5922d3'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Assign EACM Roles/button_concat(Row text contains , , ANANDK1_5922d3'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Assign EACM Roles/a_Inbox'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Assign EACM Roles/a_Inbox'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/button_katadmin'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/button_katadmin'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Logout'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Logout'))
 
 WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/input_Spring 2025_P101_COMPANY (9)'), 'kat')
 
@@ -116,24 +116,24 @@ WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/
 WebUI.setEncryptedText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/input_New_P101_PASSWORD (9)'), 
     'Ioqy8emDrxi5mkiNQATzxQ==')
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/button_Login (8)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/button_Login (8)'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Help_a-TreeView-toggle (8)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Help_a-TreeView-toggle (8)'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Enterprise Access Monitor_a-TreeView-toggle (1)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Enterprise Access Monitor_a-TreeView-toggle (1)'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_DataPaaS_a-TreeView-toggle (1)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_DataPaaS_a-TreeView-toggle (1)'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Manage Certification (1)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Manage Certification (1)'))
 
 WebUI.verifyElementNotPresent(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Certification/div_Manage Certification'), 
     0)
 
 WebUI.verifyElementNotVisible(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Certification/div_Manage Certification'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Certification/a_Inbox'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Certification/a_Inbox'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/button_anandk1'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/button_anandk1'))
 
-WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Logout_1'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Logout_1'))
 
