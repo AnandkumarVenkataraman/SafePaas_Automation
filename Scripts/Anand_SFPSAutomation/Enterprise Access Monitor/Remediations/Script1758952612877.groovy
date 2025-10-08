f463839211f42cc5e6cfe43190b43cb97cb61833
Anandkumar Venkataraman <anandkumar.venkataraman@safepaas.com.com>
2025-10-08 03:05:00 -0500

    Remediations 0810 v2

--- Scripts/Anand_SFPSAutomation/Enterprise Access Monitor/Remediations/Script1758952612877.groovy
+++ Scripts/Anand_SFPSAutomation/Enterprise Access Monitor/Remediations/Script1758952612877.groovy
@@ -32,6 +32,7 @@ WebUI.setEncryptedText(findTestObject('Object Repository/Anand_SFPSAutomation/Pa
 
 WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/button_Reset Password_loginBtn (6)'))
 
+//Actions
 WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Help_a-TreeView-toggle (21)'))
 
 WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Access Monitor_a-TreeView-toggle (12)'))
@@ -67,6 +68,68 @@ WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation
     0)
 
 WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Remediate Issues/a_Inbox_t-Button t-Button--icon t-Button--h_469732'))
+
+WebUI.acceptAlert(FailureHandling.OPTIONAL)
+
+//Manage Corrective Actions
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Help_a-TreeView-toggle (22)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Access Monitor_a-TreeView-toggle (13)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Detect Violations_a-TreeView-toggle (1)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Actions_a-TreeView-label'))
+
+WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/select_Environment_P0_ENVIRONMENT'),
+	27)
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/a_Actions_a-IRR-headerLink'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/button_Documentation_a-Button a-IRR-button _ce8d41'))
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_Actions_u-tL'),
+	'Test Rule 4', FailureHandling.OPTIONAL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_Test Rule 4_u-tL'),
+	'User Role Name -> Akhil : MCD Project Business Analyst[MCD_PJF_PROJECTS_APPLICATION_ADMINISTRATOR_JOB_CUSTOM1]', FailureHandling.OPTIONAL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_Test Rule 4_u-tL_1'),
+	'All User Roles Violations V1', FailureHandling.OPTIONAL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_All User Roles Violations V1_u-tL'),
+	'27-SEP-25', FailureHandling.OPTIONAL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_-_u-tL'),
+	'Approved', FailureHandling.OPTIONAL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_-_u-tL_1'),
+	'Approved', FailureHandling.OPTIONAL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_All User Violations V1_u-tL'),
+	'27-SEP-25', FailureHandling.OPTIONAL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_User Name - svcerpintgusr_u-tL'),
+	'All User Violations V1', FailureHandling.OPTIONAL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_Test Rule 3_u-tL'),
+	'User Name -> svcerpintgusr', FailureHandling.OPTIONAL)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/td_All User Roles Violations_u-tL'),
+	'Test Rule 3', FailureHandling.OPTIONAL)
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/img_Actions_apex-edit-page'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Modify Corrective Action/button_Modify Actions_B173567247489232319'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/img_All User Roles Violations_apex-edit-page'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Modify Corrective Action/button_Modify Actions_B173567247489232319'))
+
+WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/select_Environment_P0_ENVIRONMENT'),
+	0)
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Corrective Action/a_Inbox_t-Button t-Button--icon t-Button--h_469732'))
 
 WebUI.acceptAlert(FailureHandling.OPTIONAL)
 
