e8a140458db04afb323eab318af4b5c55f438988
Anandkumar Venkataraman <anandkumar.venkataraman@safepaas.com.com>
2025-10-10 06:35:34 -0500

    SOD Rvw Oct10 v1

--- Scripts/Anand_SFPSAutomation/Enterprise Access Monitor/SODReview/Script1759923395474.groovy
+++ Scripts/Anand_SFPSAutomation/Enterprise Access Monitor/SODReview/Script1759923395474.groovy
@@ -17,272 +17,70 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-//Comment by Anand on 08-Oct-2025
-//Pls. note -> This script will be obsolete
-//And will be retro-fitted as part of Mitigation Control
-
 WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://us21n.safepaas.com/monitor/f?p=1000:101::::::')
 
 WebUI.maximizeWindow()
 
-WebUI.setText(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Login/input_P101_COMPANY (2)'), 'KAT')
+WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/input_Summer 2025_P101_COMPANY (16)'), 'KAT')
 
-WebUI.setText(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Login/input_P101_USERNAME (2)'), 'KATALON_ADMIN')
+WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/input_OR CONTINUE WITH_P101_USERNAME (36)'), 
+    'KATADMIN')
 
-WebUI.setEncryptedText(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Login/input_P101_PASSWORD (2)'),
-	'Ioqy8emDrxi5mkiNQATzxQ==')
+WebUI.setEncryptedText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/input_New_P101_PASSWORD (36)'), 
+    'Ioqy8emDrxi5mkiNQATzxQ==')
 
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Login/button_Login (2)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Login/button_Reset Password_loginBtn (14)'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle (2)'))
+//Verify the Setups
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_DataPaaS_a-TreeView-toggle (15)'))
 
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle (2)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Administration_a-TreeView-toggle (13)'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1 (2)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Setup_a-TreeView-label'))
 
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1 (2)'))
+WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Role Groups/select_Environment_P0_ENVIRONMENT'), 
+    3)
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2 (2)'))
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Role Groups/td_Submit_t-Report-cell'), 
+    'HCM', FailureHandling.OPTIONAL)
 
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2 (2)'))
+WebUI.verifyElementText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Role Groups/input_HCM_f01'), 
+    '', FailureHandling.OPTIONAL)
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/a_Manage Remediation Plan (2)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Role Groups/button_Create Role Group_B150175829224810110'))
 
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/a_Manage Remediation Plan (2)'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Roles Mapping/span'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Actions'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Roles Mapping/button_Manage Roles Mapping_backBtn'))
 
-WebUI.click(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Actions'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Role Groups/a_Manage Role Groups_a-TreeView-label'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Filter'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Approval Assignments/a'))
 
-WebUI.click(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Filter'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Approval Assignments/span'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/select_CodeMeaningDescriptionEnabledStart D_a4cb0c'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Approval Assignments/span_1'))
 
-WebUI.selectOptionByValue(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/select_CodeMeaningDescriptionEnabledStart D_a4cb0c'),
-	'GCA_WORKFLOW', true)
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Approval Assignments/a_Manage Role Groups_a-TreeView-label'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/buttonR327963090047660484_expression_icon_link'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Approval Assignments/a_Manage Approval Workflow_a-TreeView-label'))
 
-WebUI.click(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/buttonR327963090047660484_expression_icon_link'))
+WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation/Page_SOD Review Configuration/select_Environment_P130_ENVIRONMENT'), 
+    3)
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/a_SOD Review'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_SOD Review Configuration/span'))
 
-WebUI.click(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/a_SOD Review'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_SOD Review Configuration/span_1'))
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Apply'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_SOD Review Configuration/a_Inbox_t-Button t-Button--icon t-Button--h_469732'))
 
-WebUI.click(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Apply'))
+WebUI.acceptAlert(FailureHandling.OPTIONAL)
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Actions'))
+//Violation Level
 
-WebUI.click(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Actions'))
+//Aggregate Level
 
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Filter'))
+//WebUI.closeBrowser()
 
-WebUI.click(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Filter'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/select_CodeMeaningDescriptionEnabledStart D_a4cb0c'))
-
-WebUI.selectOptionByValue(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/select_CodeMeaningDescriptionEnabledStart D_a4cb0c'),
-	'GCA_END_DATE', true)
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/select_is nullis not nullis in the lastis n_82401e'))
-
-WebUI.selectOptionByValue(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/select_is nullis not nullis in the lastis n_82401e'),
-	'is null', true)
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Apply'))
-
-WebUI.click(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/button_Apply'))
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/td_MITIGATIONS'))
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/td_Mitigations and Controls'))
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/td_11-JAN-24'))
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/deba1/EAM_Setup_ManageRemediationPlan/Page_Remediation Plan/td_SOD Review'))
-
-WebUI.takeFullPageScreenshot()
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/a_Inbox'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/a_Inbox'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle (2)'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle (2)'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1 (2)'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1 (2)'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2_3'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2_3'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_Manage SOD Review'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2_3_4'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2_3_4'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/a_Manage Review Actions'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/a_Manage Review Actions'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Manage Review Actions/select_Select any oneCERP_1CloudERPEBizApp__fcbf94'))
-
-WebUI.selectOptionByLabel(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Manage Review Actions/select_Select any oneCERP_1CloudERPEBizApp__fcbf94'),
-	'KTLN_CloudERP', false, FailureHandling.STOP_ON_FAILURE)
-
-WebUI.delay(10)
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Manage Review Actions/button_Create'))
-
-WebUI.click(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Manage Review Actions/button_Create'))
-
-WebUI.verifyElementClickable(findTestObject('deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/select_Select any oneKTLN_SODTest_Jan05_V2__533efa - 12'))
-
-WebUI.selectOptionByLabel(findTestObject('deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/select_Select any oneKTLN_SODTest_Jan05_V2__533efa - 12'),
-	'KTLN_SODTest_May27_V2', true, FailureHandling.STOP_ON_FAILURE)
-
-WebUI.verifyElementText(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/input_P181_NAME'),
-	'')
-
-WebUI.setText(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/input_P181_NAME'),
-	GlobalVariable.SODSurveyName_ViolationLevel)
-
-WebUI.verifyElementText(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/textarea_P181_DESCRIPTION'),
-	'')
-
-WebUI.setText(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/textarea_P181_DESCRIPTION'),
-	GlobalVariable.SODSurveyName_ViolationLevel)
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/button_Popup Calendar Start Date'))
-
-WebUI.click(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/button_Popup Calendar Start Date'))
-
-WebUI.verifyElementClickable(findTestObject('deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/a_22'))
-
-WebUI.click(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/a_22'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/button_Popup Calendar End Date'))
-
-WebUI.click(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/button_Popup Calendar End Date'))
-
-WebUI.verifyElementClickable(findTestObject('deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/a_23'))
-
-WebUI.click(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/a_23'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/button_Create'))
-
-WebUI.click(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/button_Create'))
-
-WebUI.takeFullPageScreenshot()
-
-WebUI.comment('This error is as expected because there are Roles which are not yet mapped/assigned to any Role-Group')
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/h2_1 error has occurred'))
-
-WebUI.verifyElementClickable(findTestObject('deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/select_Select any oneKTLN_SODTest_Jan05_V2__533efa - 12'))
-
-WebUI.selectOptionByLabel(findTestObject('deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/select_Select any oneKTLN_SODTest_Jan05_V2__533efa - 12'),
-	'KTLN_SODTest_May28_PO_AP', true, FailureHandling.OPTIONAL)
-
-WebUI.scrollToElement(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/button_Create'),
-	0)
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/button_Create'),
-	FailureHandling.STOP_ON_FAILURE)
-
-WebUI.click(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Initiate User Violation Survey/button_Create'))
-
-WebUI.delay(10)
-
-WebUI.takeFullPageScreenshot()
-
-WebUI.comment('SOD Survey created')
-
-WebUI.click(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Manage Review Actions/button_t-Button t-Button--noUI t-Button--ic_cbe08f'),
-	FailureHandling.OPTIONAL)
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/deba1/ManageRemediationPlan/Page_Manage Review Actions/a_Status'),
-	FailureHandling.OPTIONAL)
-
-WebUI.delay(2)
-
-WebUI.verifyElementClickable(findTestObject('deba3/ManageReviewStatus/Page_Home/a_Inbox_1'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/a_Inbox_1'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle (2)'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle (2)'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1 (2)'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1 (2)'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2_3'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2_3'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2_3_4'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/span_a-TreeView-toggle_1_2_3_4'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/a_Manage Review Status'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Home/a_Manage Review Status'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/select_Select any oneCERP_1CloudERPEBizApp__fcbf94'))
-
-WebUI.selectOptionByLabel(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/select_Select any oneCERP_1CloudERPEBizApp__fcbf94'),
-	'KTLN_CloudERP', false, FailureHandling.STOP_ON_FAILURE)
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/button_Actions'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/button_Actions'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/button_Filter'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/span_Filter'))
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/select_NameDescriptionStatusStart DateEnd D_54845d'))
-
-WebUI.verifyElementText(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/input_p_ignore_10'),
-	'')
-
-WebUI.setText(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/input_p_ignore_10'),
-	GlobalVariable.SODSurveyName_ViolationLevel)
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/button_Apply'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/button_Apply'))
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/span_1 -                    1'),
-	FailureHandling.OPTIONAL)
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/a_KTLN_SODReview_Jan22_V2'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_Manage Review Status/a_KTLN_SODReview_Jan22_V2'))
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_User Access Validation/h2_User Access'),
-	FailureHandling.OPTIONAL)
-
-WebUI.verifyElementVisible(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_User Access Validation/h2_Open'),
-	FailureHandling.OPTIONAL)
-
-WebUI.verifyElementClickable(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_User Access Validation/button_Cancel'))
-
-WebUI.click(findTestObject('Object Repository/deba3/ManageReviewStatus/Page_User Access Validation/button_Cancel'))
-
-WebUI.verifyElementVisible(findTestObject('deba3/ManageReviewStatus/Page_Manage Review Status/a_KTLN_SODReview_Jan22_V2'),
-	FailureHandling.OPTIONAL)
-
-WebUI.closeBrowser()
