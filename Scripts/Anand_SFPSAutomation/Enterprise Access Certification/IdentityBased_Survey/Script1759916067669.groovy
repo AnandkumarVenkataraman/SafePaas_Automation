55d25573a001fc19e8b7d93f283af9297c14bd69
Anandkumar Venkataraman <anandkumar.venkataraman@safepaas.com.com>
2025-10-13 00:06:33 -0500

    Identity UAR v1

--- Scripts/Anand_SFPSAutomation/Enterprise Access Certification/IdentityBased_Survey/Script1759916067669.groovy
+++ Scripts/Anand_SFPSAutomation/Enterprise Access Certification/IdentityBased_Survey/Script1759916067669.groovy
@@ -163,33 +163,6 @@ WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage User Access/bu
 WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage User Access/a_Inbox_t-Button t-Button--icon t-Button--h_469732'))
 
 //Launch the Identity UAR and certify the records
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.ConditionType as ConditionType
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
-import java.text.SimpleDateFormat as SimpleDateFormat
-import java.util.Date as Date
-import java.time.LocalDate as LocalDate
-import java.time.format.DateTimeFormatter as DateTimeFormatter
-
-WebUI.openBrowser('')
-
-WebUI.navigateToUrl('https://us21n.safepaas.com/monitor/f?p=1000:101::::::')
 
 WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/button_Inbox_L14205299837764170 (13)'))
 
@@ -262,7 +235,7 @@ WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/spa
 WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Manage Certification_a-TreeView-label (3)'))
 
 WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage User Access/input_User Access_userAccessReport_search_field (3)'),
-	'Identity_UAR')
+	singleEnvUAR_ID)
 
 WebUI.click(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage User Access/button_User Access_userAccessReport_search_button (2)'))
 
