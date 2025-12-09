45771da30f612967eb0020c004fea31129003b29
Anandkumar Venkataraman <anandkumar.venkataraman@safepaas.com.com>
2025-12-09 05:47:29 -0600

    Test Suite v2

--- Test Suites/SafePaaS_Automation/SAP SOD.ts
+++ Test Suites/SafePaaS_Automation/SAP SOD.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description>TS_SFPSAutomation</description>
-   <name>TS_SFPSAutomation</name>
+   <name>SAP SOD</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
