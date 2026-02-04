2632c71a83c33e1ecec1a3d5d88b5d5f0a5008d1
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2026-02-04 00:02:17 -0600

    IQA Smoke test suite v1

--- Test Suites/SafePaaS_Automation/IQA Weekly Runs.ts
+++ Test Suites/SafePaaS_Automation/IQA Weekly Runs.ts
@@ -20,7 +20,7 @@
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
+            <profileName>IQA</profileName>
             <requireConfigurationData>false</requireConfigurationData>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
