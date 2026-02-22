5e1f7a8288c689306cee15d473323d2d452cbbc2
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2026-02-22 00:18:48 -0600

    Test Suite Feb22 v1

---
+++ Test Suites/SFPS_Automation/1. Administration.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Administration</description>
+   <name>1. Administration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>77bd2dca-fc18-48fc-8dcf-a2475c2ba99f</testSuiteGuid>
+   <testCaseLink>
+      <guid>3934d924-6fc1-4c64-8729-b93338c11cee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SFPSAutomation/Administration/Administration_Main</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/SFPS_Automation/2. DataProbe.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>DataProbe</description>
+   <name>2. DataProbe</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a482eaea-6fba-4521-a3f4-5db4be4d3ea8</testSuiteGuid>
+   <testCaseLink>
+      <guid>1b4ee811-820b-481f-95b4-c90d4ee90c09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SFPSAutomation/DataProbe/DataProbe_Main IQA</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
