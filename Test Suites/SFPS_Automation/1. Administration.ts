e639ddbaa75e11aa1131df2de87e2a397a8a658a
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2026-02-22 01:59:13 -0600

    EACM Feb22 v3

--- Test Suites/SFPS_Automation/1. Administration.ts
+++ Test Suites/SFPS_Automation/1. Administration.ts
@@ -12,87 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>77bd2dca-fc18-48fc-8dcf-a2475c2ba99f</testSuiteGuid>
    <testCaseLink>
-      <guid>b83b231d-6e2f-410a-87d3-251d352ff6fa</guid>
+      <guid>cce24f73-1e9b-48ad-a0d6-8fe9ea4f9795</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/SFPSAutomation/Administration/1. Manage Company</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ab3bf60d-0b6f-4c88-a17a-84608f495305</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/2. Manage Environment</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>afc2201d-fcbe-4808-b92b-f155d135e46e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/3. Manage Enterprise IT Map</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>718e7923-36ae-4d76-a023-8fe8849b609f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/4. Manage Security Positions</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>1b1513bf-dccf-4222-bdd7-442f32127e5b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/5. SafePaaS User Listing Report</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>5de10280-e90d-4827-b560-1f56b4ff129f</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/6. SafePaaS User Application Listing Report</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>e6264873-d10d-4722-8ec2-e312fd675d4b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/7. Scheduler</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>a760e7d7-9747-4a50-b831-00ea64e7f5d6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/8. Scheduled Downloads</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ba315652-cb37-47ff-9764-102a1e9a4926</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/9. Log Report</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>0a9e221a-834a-49bb-a0a0-b96536ff9f0c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/10. Usage</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>013a85fb-e2ee-4d5a-941b-8d3e465394df</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/11. Release Update</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>e58d1baf-5229-4c87-b306-6743749021eb</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SFPSAutomation/Administration/12. Security</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
