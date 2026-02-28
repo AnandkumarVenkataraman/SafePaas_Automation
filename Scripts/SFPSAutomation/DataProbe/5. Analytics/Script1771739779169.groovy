5e730c9ce0eaaca381c631b01dbdbc03db558bf0
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2026-02-28 00:17:20 -0600

    Application Access Reports in EAM Analytics, after DTP Snapshot is run v1

--- Scripts/SFPSAutomation/DataProbe/5. Analytics/Script1771739779169.groovy
+++ Scripts/SFPSAutomation/DataProbe/5. Analytics/Script1771739779169.groovy
@@ -21,141 +21,140 @@ import java.util.Date as Date
 import java.time.LocalDate as LocalDate
 import java.time.format.DateTimeFormatter as DateTimeFormatter
 
-//Analytics
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Help_a-TreeView-toggle (2)'))
+WebUI.openBrowser('')
 
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Access Monitor_a-TreeView-toggle (2)'))
+WebUI.navigateToUrl(GlobalVariable.URL) //IQA or Prod. Profile should be selected during execution
 
-//WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Dashboard_a-TreeView-toggle')) //Prod.
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Dashboard_a-TreeView-toggle') //IQA
-    )
+WebUI.maximizeWindow()
 
-WebUI.delay(2)
+WebUI.setText(findTestObject('Object Repository/Page_Login/input_Summer 2025_P101_COMPANY (1)'), 'KAT')
 
-//WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Violation Report_a-TreeView-toggle')) //Prod.
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Violation Report_a-TreeView-toggle') //IQA
-    )
+WebUI.setText(findTestObject('Object Repository/Page_Login/input_OR CONTINUE WITH_P101_USERNAME (6)'), 'KATADMIN')
 
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/a_Application Access Report_a-TreeView-label'))
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Login/input_New_P101_PASSWORD (6)'), 'Ioqy8emDrxi5mkiNQATzxQ==')
 
-WebUI.selectOptionByLabel(findTestObject('Object Repository/Page_User Listing/select_Environment_P0_ENVIRONMENT'), 'EBizApp_JDBC_Env', 
-    false)
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Login/button_Login (6)'))
 
-WebUI.enhancedClick(findTestObject('Object Repository/Page_User Listing/a_User Listing and access_a-TreeView-label'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Help_a-TreeView-toggle (94)'))
 
-WebUI.delay(2)
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Access Monitor_a-TreeView-toggle (70)'))
 
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Analytics_a-TreeView-toggle (51)'))
+//Attributes
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Manage Rules (8)'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Rules/select_Select any oneCoupa_Env1EBizApp_JDBC_743da1 (1)'),
+	'5888', true)
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Rules/button_Filter False - Positives (3)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Filter False-Positives/input_Exclude  Unexclude All_control_298068_f41ab9'))
+WebUI.delay(5)
 WebUI.takeScreenshot()
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Filter False-Positives/input_Exclude  Unexclude All_control_298068_f41ab9'))
 
-WebUI.enhancedClick(findTestObject('Object Repository/Page_User Entry Access Listing/a_User Entry Access Listing_a-TreeView-label'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Filter False-Positives/button_Cancel (3)'))
 
-WebUI.delay(2)
+WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Rules/select_Select any oneCoupa_Env1EBizApp_JDBC_743da1 (1)'),
+	0)
 
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Rules/a_Inbox (4)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Help_a-TreeView-toggle (94)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Access Monitor_a-TreeView-toggle (70)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Dashboard_a-TreeView-toggle (29)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Violation Report_a-TreeView-toggle (3)'))
+//Application Access Reports - all EAM Security objects
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_User Listing and access'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_User Listing/select_Select any oneCoupa_Env1EBizApp_JDBC_743da1'),
+	'5888', true)
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_User Listing/a_User Entry Access Listing (1)'))
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_User Entry Attribute Access Listing/a_User Entry Attribute Access Listing_a-Tre_258751'))
-
-WebUI.delay(2)
-
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_User Entry Access Listing/a_User Entry Attribute Access Listing (1)'))
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.selectOptionByLabel(findTestObject('Object Repository/Page_Entry Listing/select_Entry Type_P76_ENTRY_TYPE'), 'Responsibility', 
-    false)
-
-WebUI.delay(2)
-
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_User Entry Attribute Access Listing/a_Entry Listing (1)'))
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.selectOptionByLabel(findTestObject('Object Repository/Page_Entry Listing/select_Entry Type_P76_ENTRY_TYPE'), 'Menu', 
-    false)
-
-WebUI.delay(2)
-
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Entry Listing/select_Select any oneResponsibilityMenuFunc_024d81 (1)'),
+	'21', true)
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.selectOptionByLabel(findTestObject('Object Repository/Page_Entry Listing/select_Entry Type_P76_ENTRY_TYPE'), 'Function', 
-    false)
-
-WebUI.delay(2)
-
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Entry Listing/select_Select any oneResponsibilityMenuFunc_024d81 (1)'),
+	'480', true)
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.selectOptionByLabel(findTestObject('Object Repository/Page_Entry Listing/select_Entry Type_P76_ENTRY_TYPE'), 'Grant', 
-    false)
-
-WebUI.delay(2)
-
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Entry Listing/select_Select any oneResponsibilityMenuFunc_024d81 (1)'),
+	'481', true)
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.selectOptionByLabel(findTestObject('Object Repository/Page_Entry Listing/select_Entry Type_P76_ENTRY_TYPE'), 'Concurrent Program', 
-    false)
-
-WebUI.delay(2)
-
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Entry Listing/select_Select any oneResponsibilityMenuFunc_024d81 (1)'),
+	'482', true)
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.selectOptionByLabel(findTestObject('Object Repository/Page_Entry Listing/select_Entry Type_P76_ENTRY_TYPE'), 'Role', 
-    false)
-
-WebUI.delay(2)
-
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Entry Listing/select_Select any oneResponsibilityMenuFunc_024d81 (1)'),
+	'483', true)
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.selectOptionByLabel(findTestObject('Object Repository/Page_Entry Listing/select_Entry Type_P76_ENTRY_TYPE'), 'Request Group', 
-    false)
-
-WebUI.delay(2)
-
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Entry Listing/select_Select any oneResponsibilityMenuFunc_024d81 (1)'),
+	'582', true)
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Entry Listing/a_Entry Listing_a-TreeView-label'))
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Entry Attributes/button_Entry Type_a-Button a-IRR-button a-B_36387a'), 
-    FailureHandling.OPTIONAL)
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Entry Attributes/button_Entry Type_a-Button a-IRR-button a-B_36387a'), 
-    FailureHandling.OPTIONAL)
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Entry Attributes/button_Entry Type_a-Button a-IRR-button a-B_36387a'), 
-    FailureHandling.OPTIONAL)
-
-WebUI.delay(2)
-
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Entry Listing/select_Select any oneResponsibilityMenuFunc_024d81 (1)'),
+	'584', true)
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Entry Attributes/a_Entry Attributes_a-TreeView-label'))
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Access Hierarchy/button_Access Type_P13_RESPONSIBILITY_lov_btn'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Access Hierarchy/input_Search_a-PopupLOV-search apex-item-text'), 'General Ledger Super User')
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Access Hierarchy/button_Search_a-Button a-PopupLOV-doSearch'))
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Access Hierarchy/li_Select List 20 rows. Use arrow keys_a-Ic_89dcca'))
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Access Hierarchy/button_Access Type_P13_GO'))
-
-WebUI.delay(2)
-
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Entry Listing/a_Entry Attributes (1)'))
+WebUI.delay(5)
 WebUI.takeScreenshot()
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Access Hierarchy/span_SOD Review Survey_a-TreeView-toggle'))
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Access Hierarchy/a_Define Scope_a-TreeView-label'))
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Rules/button_Export Rules_B237699209801060311'))
-
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Filter False-Positives/label_Report Settings_a-IRR-controlsCheckboxLabel'))
-
-WebUI.delay(2)
-
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Entry Attributes/a_Access Hierarchy (1)'))
+WebUI.delay(5)
 WebUI.takeScreenshot()
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Access Hierarchy/button_Access Type_P13_RESPONSIBILITY_lov_btn (2)'))
+WebUI.delay(5)
+WebUI.takeScreenshot()
+WebUI.setText(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Access Hierarchy/input_Search_a-PopupLOV-search apex-item-text (2)'),
+	'General Ledger Super User (Process Operations)[GL_VISION_PROCESS] (RESPONSIBILITY)')
 
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Filter False-Positives/label_Report Settings_a-IRR-controlsCheckboxLabel'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Access Hierarchy/button_Search_a-Button a-PopupLOV-doSearch (2)'))
 
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Filter False-Positives/button_Submit_B245973424258406093'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Access Hierarchy/span_General Ledger Super User'))
 
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Rules/a_Inbox_t-Button t-Button--icon t-Button--h_469732'))
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Access Hierarchy/button_Go (1)'))
+WebUI.delay(5)
+WebUI.takeScreenshot()
+WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Access Hierarchy/select_Select any oneCoupa_Env1EBizApp_JDBC_743da1'),
+	0)
 
-WebUI.callTestCase(findTestCase('SFPSAutomation/DataProbe/6. Synchronization'), [:])
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Access Hierarchy/a_Inbox (1)'))
+//Application Role Group
 
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Help_a-TreeView-toggle (95)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Enterprise Access Certification_a-Tree_1e8bd7 (7)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/span_Manage My Access_a-TreeView-toggle (2)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Manage Role Groups'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Role Groups/select_Select any oneCoupa_Env1EBizApp_JDBC_73bea7'),
+	'5888', true)
+
+WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Role Groups/select_Select any oneCoupa_Env1EBizApp_JDBC_73bea7'),
+	0)
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Manage Role Groups/a_Inbox'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/button_katadmin (26)'))
+
+WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_Logout (34)'))
+
+WebUI.closeBrowser()
+
+//ENd
