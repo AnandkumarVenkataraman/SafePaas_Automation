650b21cb5c856163acbe2d668e511632263faa95
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2026-03-03 03:16:48 -0600

    DPS MTP - Test suie v1

---
+++ Test Suites/SFPS_Automation/MonitorPaaS and DataPaaS.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>MonitorPaaS and DataPaaS</description>
+   <name>MonitorPaaS and DataPaaS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fbe4af21-4782-42e7-8f8d-21ab2f033a29</testSuiteGuid>
+   <testCaseLink>
+      <guid>24d0c6a5-ec54-4f2d-9bfc-f5270ec15fb2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SFPSAutomation/DataPaaS/DPS_MTP/1. Manage Object - Use Snapshot</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a19b5a40-1ad7-4e60-ba56-a374e77896dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SFPSAutomation/DataPaaS/DPS_MTP/2. Manage Object - Use Triggers</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf394cb5-2ac2-44cd-b98e-661e86eb5496</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SFPSAutomation/DataPaaS/DPS_MTP/3. Manage Object - Transformation Rule</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>20b31ddb-6644-409c-aef2-6cb72aab2681</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SFPSAutomation/DataPaaS/DPS_MTP/4. Manage Object Set</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
