007da9d12e72bd66d89c5bf9b3bb20909db86b50
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2026-03-27 00:43:05 -0500

    Sample Demo

---
+++ Test Suites/SampleDemo/SampleDemo.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SampleDemo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9a068548-83a5-4009-a7d0-f0b21ebc24b5</testSuiteGuid>
+   <testCaseLink>
+      <guid>43cd96b3-bf46-4566-b78b-7f45d37417a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SFPSAutomation/Other/AlexFolder/Sample_Demo2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
