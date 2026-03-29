888782f6a7d4318632350727765a85579564d1b8
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2026-03-29 04:15:48 -0500

    EACM10 Mar30 v1

--- Scripts/SFPSAutomation/Enterprise Access Certification/10. Scheduler/Script1771750722958.groovy
+++ Scripts/SFPSAutomation/Enterprise Access Certification/10. Scheduler/Script1771750722958.groovy
@@ -23,8 +23,19 @@ import java.time.LocalDate as LocalDate
 import java.time.format.DateTimeFormatter as DateTimeFormatter
 import com.kms.katalon.core.util.KeywordUtil
 
-try
-{
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl(GlobalVariable.URL) //IQA or Prod. Profile should be selected during execution
+
+WebUI.maximizeWindow()
+
+WebUI.setText(findTestObject('Object Repository/Page_Login/input_Summer 2025_P101_COMPANY (3)'), 'KAT')
+
+WebUI.setText(findTestObject('Object Repository/Page_Login/input_OR CONTINUE WITH_P101_USERNAME (8)'), 'KATADMIN')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Login/input_New_P101_PASSWORD (8)'), 'Ioqy8emDrxi5mkiNQATzxQ==')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Login/button_Reset Password_loginBtn (1)'))
 //Verify in Scheduler
 WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_DataPaaS_a-TreeView-toggle (11)'))
 
@@ -35,18 +46,13 @@ WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/a_Platform_a-Tre
 WebUI.delay(20)
 
 WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Scheduler/select_Job Time_P29_JOB_TIME'),
-	1)
-
-WebUI.delay(20)
-
-WebUI.selectOptionByIndex(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Scheduler/select_Job Time_P29_JOB_TIME'),
 	0)
 
 WebUI.delay(20)
 
 WebUI.enhancedClick(findTestObject('Object Repository/Page_Scheduler/input_Job Time_SCHEDULER_search_field (1)'))
 
-WebUI.setText(findTestObject('Object Repository/Page_Scheduler/input_Job Time_SCHEDULER_search_field (1)'), actualUACId,
+WebUI.setText(findTestObject('Object Repository/Page_Scheduler/input_Job Time_SCHEDULER_search_field (1)'), 'FSOD_INITIATE_UAC',
 	FailureHandling.OPTIONAL)
 
 WebUI.delay(20)
@@ -62,9 +68,5 @@ WebUI.enhancedClick(findTestObject('Object Repository/Page_Scheduler/button_conc
 
 WebUI.enhancedClick(findTestObject('Object Repository/Page_Scheduler/a_Inbox_t-Button t-Button--icon t-Button--h_469732 (1)'),
 	FailureHandling.OPTIONAL)
-//WebUI.callTestCase(findTestCase, null)
-}
-catch(Exception e) {
-	WebUI.comment("Exception is: " + e.getMessage())
-	KeywordUtil.markPassed("")
-}
+
+WebUI.closeBrowser()
