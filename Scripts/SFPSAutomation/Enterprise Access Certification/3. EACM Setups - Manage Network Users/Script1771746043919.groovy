2bf43454f02b39bb408fb9816c72c026c4bb77d7
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2026-03-29 00:57:29 -0500

    EACM Mar30 v1

--- Scripts/SFPSAutomation/Enterprise Access Certification/3. EACM Setups - Manage Network Users/Script1771746043919.groovy
+++ Scripts/SFPSAutomation/Enterprise Access Certification/3. EACM Setups - Manage Network Users/Script1771746043919.groovy
@@ -1,30 +1,35 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.ConditionType as ConditionType
 import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-import java.text.SimpleDateFormat as SimpleDateFormat
-import java.util.Date as Date
-import java.time.LocalDate as LocalDate
-import java.time.format.DateTimeFormatter as DateTimeFormatter
-import com.kms.katalon.core.util.KeywordUtil
 
-try
-{
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl(GlobalVariable.URL) //IQA or Prod. Profile should be selected during execution
+
+WebUI.maximizeWindow()
+
+WebUI.setText(findTestObject('Object Repository/Page_Login/input_Summer 2025_P101_COMPANY (3)'), 'KAT')
+
+WebUI.setText(findTestObject('Object Repository/Page_Login/input_OR CONTINUE WITH_P101_USERNAME (8)'), 'KATADMIN')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Login/input_New_P101_PASSWORD (8)'), 'Ioqy8emDrxi5mkiNQATzxQ==')
+
+WebUI.enhancedClick(findTestObject('Object Repository/Page_Login/button_Reset Password_loginBtn (1)'))
+
 WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Help_a-TreeView-toggle (8)'))
 
 WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Enterprise Access Monitor_a-TreeView-toggle'))
@@ -33,23 +38,12 @@ WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/span_Access Cert
 
 WebUI.enhancedClick(findTestObject('Object Repository/Page_Home/a_Setup_a-TreeView-label (2)'))
 
-WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Manage Network Users/select_Environment_P0_ENVIRONMENT'),
-	'9148', true)
-
+WebUI.selectOptionByLabel(findTestObject('Object Repository/Page_Manage Network Users/select_Environment_P0_ENVIRONMENT'),
+	'EBizApp_JDBC_Env', false)
 WebUI.selectOptionByIndex(findTestObject('Object Repository/Page_Manage Network Users/select_Environment_P0_ENVIRONMENT'),
 	0)
 
-WebUI.enhancedClick(findTestObject('Object Repository/Page_Manage Network Users/a_Manage Network Users_a-TreeView-label'))
-
-WebUI.selectOptionByIndex(findTestObject('Object Repository/Page_Manage Access Groups/select_Environment_P0_ENVIRONMENT'),
-	1)
-
-WebUI.selectOptionByIndex(findTestObject('Object Repository/Page_Manage Access Groups/select_Environment_P0_ENVIRONMENT'),
-	2)
-
+//WebUI.selectOptionByIndex(findTestObject('Object Repository/Page_Manage Network Users/select_Environment_P0_ENVIRONMENT'),
+///	0)
+WebUI.closeBrowser()
 //WebUI.callTestCase(findTestCase, null)
-}
-catch(Exception e) {
-	WebUI.comment("Exception is: " + e.getMessage())
-	KeywordUtil.markPassed("")
-}
