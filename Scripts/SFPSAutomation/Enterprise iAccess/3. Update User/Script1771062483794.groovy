d88449b36c24dbca1481090ae6e9509dafc6e0c3
Anandkumar Venkatara <Anandkumar Venkatara@APP05-KATALON>
2026-04-02 00:11:28 -0500

    EIA Apr2 v1

--- Scripts/SFPSAutomation/Enterprise iAccess/3. Update User/Script1771062483794.groovy
+++ Scripts/SFPSAutomation/Enterprise iAccess/3. Update User/Script1771062483794.groovy
@@ -18,9 +18,6 @@ import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 
-try
-{
-
 WebUI.openBrowser('')
 
 WebUI.navigateToUrl(GlobalVariable.URL)
@@ -821,8 +818,3 @@ WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_
 WebUI.enhancedClick(findTestObject('Object Repository/Anand_SFPSAutomation/Page_Home/a_My Profile_menu_L14205299837764170_1i_7'))
 
 WebUI.closeBrowser()
-}
-catch(Exception e) {
-	WebUI.comment("Exception is: " + e.getMessage())
-	KeywordUtil.markPassed("")
-}
